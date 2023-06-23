7025fde471b55726807ea30f86be0bc123ce7e20
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-06-23 07:47:13 +0700

    starter

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import reactFacts from "../assets/react-icon-small.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={reactFacts} />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+}
